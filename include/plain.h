a982814f42fca9d1e7e33e6a7f2acf51bbeb18e3
Ivan Yakovenko <32681800+meyakovenkoj@users.noreply.github.com>
2022-10-13 11:09:32 +0300

    remove useless function

--- include/plain.h
+++ include/plain.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int plain(int binary_mod, FILE *fp1, FILE *fp2, int count, int length, float *result);
 
 void clean_process(float *array1, float *array2, int count, int length, float *result);
 
--- src/plain/plain.c
+++ src/plain/plain.c
@@ -23,20 +23,6 @@ float process(int size, float *first_arr, float *second_arr)
     return cosine;
 }
 
-int plain(int binary_mod, FILE *fp1, FILE *fp2, int count, int length, float *result)
-{
-    float *array1 = malloc(length);
-    float *array2 = malloc(length);
-    for (int k = 0; k < count; k++) {
-        ReadData(binary_mod, length, &array1, fp1);
-        ReadData(binary_mod, length, &array2, fp2);
-        result[k] = process(length, array1, array2);
-    }
-    free(array1);
-    free(array2);
-    return 0;
-}
-
 void clean_process(float *array1, float *array2, int count, int length, float *result)
 {
     for (int k = 0; k < count; k++) {
