c83b328ae7f49fb281cce5f7a4dfd99c57c6965e
Ivan Yakovenko <32681800+meyakovenkoj@users.noreply.github.com>
2022-10-22 01:16:08 +0300

    improve test.sh

--- test.sh
+++ test.sh
@@ -3,13 +3,19 @@ set -x
 
 usage() { echo "Usage: $0 -t <test.bin> [-r]" 1>&2; exit 1; }
 
-while getopts ":rt:" o; do
+while getopts ":rmgt:" o; do
     case "${o}" in
         r)
             MPIRUNFL=--allow-run-as-root
             ;;
         t)
             TESTBIN=${OPTARG}
+            ;;
+        m)
+            MPIRUN=mpirun
+            ;;
+        g)
+            NOGEN=1
             ;;
         *)
             usage
@@ -22,16 +28,16 @@ if [ -z "${TESTBIN}" ]; then
     usage
 fi
 
-
+if [ -z "${NOGEN}" ]; then
 ./gen.bin -s 100 -l 100
 ./gen.bin -s 200 -l 200
 ./gen.bin -s 400 -l 400
 ./gen.bin -s 800 -l 800
 ./gen.bin -s 1600 -l 1600
+fi
 
-
-mpirun $MPIRUNFL ./${TESTBIN} -s 26214400 -l 100 gen_0100mb_1.txt gen_0100mb_2.txt
-mpirun $MPIRUNFL ./${TESTBIN} -s 52428800 -l 200 gen_0200mb_1.txt gen_0200mb_2.txt
-mpirun $MPIRUNFL ./${TESTBIN} -s 104857600 -l 400 gen_0400mb_1.txt gen_0400mb_2.txt
-mpirun $MPIRUNFL ./${TESTBIN} -s 209715200 -l 800 gen_0800mb_1.txt gen_0800mb_2.txt
-mpirun $MPIRUNFL ./${TESTBIN} -s 419430400 -l 1600 gen_1600mb_1.txt gen_1600mb_2.txt
+$MPIRUN $MPIRUNFL ./${TESTBIN} -s 26214400 -l 100 gen_0100mb_1.txt gen_0100mb_2.txt
+$MPIRUN $MPIRUNFL ./${TESTBIN} -s 52428800 -l 200 gen_0200mb_1.txt gen_0200mb_2.txt
+$MPIRUN $MPIRUNFL ./${TESTBIN} -s 104857600 -l 400 gen_0400mb_1.txt gen_0400mb_2.txt
+$MPIRUN $MPIRUNFL ./${TESTBIN} -s 209715200 -l 800 gen_0800mb_1.txt gen_0800mb_2.txt
+$MPIRUN $MPIRUNFL ./${TESTBIN} -s 419430400 -l 1600 gen_1600mb_1.txt gen_1600mb_2.txt
